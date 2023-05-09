b4660f3b75eb0d4a92efe45508e1ce2276e4082e
“Omar <ocamacho812@gmail.com>
2023-05-08 18:56:14 -0700

    Fix config.ts file

--- server/src/config.ts
+++ server/src/config.ts
@@ -1,36 +1,29 @@
-// Define the required options for connecting to Auth0
 interface Auth0Options {
-  domain: string | undefined; // The domain of your Auth0 account
-  clientId: string | undefined; // Your Auth0 client ID
-  clientSecret: string | undefined; // Your Auth0 client secret
-  callbackURL: string | undefined; // The URI that Auth0 should redirect to after authentication
+  domain: string | undefined;
+  clientId: string | undefined;
+  clientSecret: string | undefined;
+  callbackURL: string | undefined;
 }
 
-// Define the options for JWT tokens
 interface JwtOptions {
-  secretKey: string | undefined; // The shared secret key used to sign JWT tokens
-  expiresIn: string; // The amount of time for which a JWT token is valid
+  secretKey: string | undefined;
+  expiresIn: string;
 }
 
-// Define the overall configuration options for the app
 interface ConfigOptions {
-  mongodb_uri: string | undefined; // The URI for your MongoDB database
-  auth0: Auth0Options; // An object containing the required Auth0 options
-  jwt: JwtOptions; // An object containing the required JWT options
+  mongodb_uri: string | undefined;
+  auth0: Auth0Options;
+  jwt: JwtOptions;
 }
 
-// Export the complete configuration object
 export const config: ConfigOptions = {
-  // Get the MongoDB URI from environment variables
-  mongodb_uri: process.env.MONGODB_URI,
-  // Get the Auth0 domain, client ID, client secret, and callback URL from environment variables
+  mongodb_uri: process.env.MONGO_URI,
   auth0: {
     domain: process.env.AUTH0_DOMAIN,
     clientId: process.env.AUTH0_CLIENT_ID,
     clientSecret: process.env.AUTH0_CLIENT_SECRET,
     callbackURL: process.env.AUTH0_CALLBACK_URL
   },
-  // Get the JWT secret key and expiration time from environment variables
   jwt: {
     secretKey: process.env.JWT_SECRET,
     expiresIn: '1h'
--- server/src/index.ts
+++ server/src/index.ts
@@ -2,28 +2,33 @@ import dotenv from 'dotenv';
 import cors from 'cors';
 import { waitForMongoConnection } from './db';
 import { setupPassportSession } from './services/passport.service';
-// import authRouter from './routes/auth.router';
+
+// Import authentication router
 const authRouter = require('./routes/auth.router')
 
+// Load environment variables from .env file
 dotenv.config();
 
+// Initialize the Express application
 const express = require('express');
-
 const PORT = process.env.PORT || 3001;
-
 const app = express();
 
+// Enable Cross-Origin Resource Sharing (CORS) and JSON parsing middleware
 app.use(cors());
 app.use(express.json());
+
+// Mount the authentication router on a specific path
 // app.use('/', authRouter)
 
+// Start the server and set up Passport session
 async function startServer() {
   await waitForMongoConnection();
   app.listen(PORT, () => {
     console.log(`Server listening on ${PORT}`);
   });
-
   setupPassportSession(app);
 }
 
+// Call the main function to start the server
 startServer().catch((err) => console.error(err));
