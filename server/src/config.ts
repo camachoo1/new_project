cd1ca57ccce610265e05c79dac3b6d394228ab52
“Omar <ocamacho812@gmail.com>
2023-05-08 18:09:10 -0700

    Add comments to config

--- server/src/config.ts
+++ server/src/config.ts
@@ -1,29 +1,36 @@
+// Define the required options for connecting to Auth0
 interface Auth0Options {
-  domain: string | undefined;
-  clientId: string | undefined;
-  clientSecret: string | undefined;
-  callbackURL: string | undefined;
+  domain: string | undefined; // The domain of your Auth0 account
+  clientId: string | undefined; // Your Auth0 client ID
+  clientSecret: string | undefined; // Your Auth0 client secret
+  callbackURL: string | undefined; // The URI that Auth0 should redirect to after authentication
 }
 
+// Define the options for JWT tokens
 interface JwtOptions {
-  secretKey: string | undefined;
-  expiresIn: string;
+  secretKey: string | undefined; // The shared secret key used to sign JWT tokens
+  expiresIn: string; // The amount of time for which a JWT token is valid
 }
 
+// Define the overall configuration options for the app
 interface ConfigOptions {
-  mongodb_uri: string | undefined;
-  auth0: Auth0Options;
-  jwt: JwtOptions;
+  mongodb_uri: string | undefined; // The URI for your MongoDB database
+  auth0: Auth0Options; // An object containing the required Auth0 options
+  jwt: JwtOptions; // An object containing the required JWT options
 }
 
+// Export the complete configuration object
 export const config: ConfigOptions = {
+  // Get the MongoDB URI from environment variables
   mongodb_uri: process.env.MONGODB_URI,
+  // Get the Auth0 domain, client ID, client secret, and callback URL from environment variables
   auth0: {
     domain: process.env.AUTH0_DOMAIN,
     clientId: process.env.AUTH0_CLIENT_ID,
     clientSecret: process.env.AUTH0_CLIENT_SECRET,
     callbackURL: process.env.AUTH0_CALLBACK_URL
   },
+  // Get the JWT secret key and expiration time from environment variables
   jwt: {
     secretKey: process.env.JWT_SECRET,
     expiresIn: '1h'
--- server/src/models/user.model.ts
+++ server/src/models/user.model.ts
@@ -1,21 +1,26 @@
-import mongoose, { Schema, Document } from 'mongoose'
+import mongoose, { Schema, Document } from 'mongoose';
 
+// Define the properties that can be used to create or update a User document
 export interface UserDocument extends Document {
   auth0Id: string;
   email: string;
   username: string
 }
 
+// Define the structure of a User document and its validation constraints
 const UserSchema = new Schema({
+  // An auth0 ID is required to uniquely identify a user
   auth0Id: {
     type: String,
     required: true
   },
+  // An email address is required and must be unique
   email: {
     type: String,
     required: true,
     unique: true
   },
+  // A username is required and must be unique
   username: {
     type: String,
     required: true,
@@ -23,4 +28,5 @@ const UserSchema = new Schema({
   }
 })
 
+// Creates a User model using the defined schema and exports it for use in other parts of the application
 export const User = mongoose.model<UserDocument>('User', UserSchema)
